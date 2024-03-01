67ed127818fcaa35b7eab0053443501b2001c0d4
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2024-03-02 03:41:29 +0600

    save changes

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -25,7 +25,11 @@ class UserController extends Controller
         $result = $this->userService->saveData($data, config('app.user'));
         if($result['code'] == 200)
         {
-            return response(['data' => $result['user']]);
+            return response(['data' => [
+                "user" => $result['user'],
+                "user_data" => $result['user_data'],
+                "tags" => $result['user']->tags
+            ]]);
         }
         return response(['message' => $result['message']])->setStatusCode($result['code']);
     }
@@ -33,7 +37,11 @@ class UserController extends Controller
     public function getData()
     {
         $user = User::where('id', config('app.user'))->first();
-        return response(['data' => $user]);
+        return response(['data' => [
+            "user" => $user,
+            "user_data" => $user->data,
+            "tags" => $user->tags
+        ]]);
 
     }
 }
