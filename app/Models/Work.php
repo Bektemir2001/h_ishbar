075d74ebbe6aef113cfb751f6281bc6017732536
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2024-03-02 11:58:08 +0600

    save changes

--- app/Models/Work.php
+++ app/Models/Work.php
@@ -10,4 +10,15 @@ class Work extends Model
     use HasFactory;
     protected $table = 'works';
     protected $guarded = false;
+
+    static $STATUS = ['OPEN', 'CLOSED', 'FINISHED'];
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'work_tags');
+    }
+
+    public function getStatusAttribute($value)
+    {
+        return self::$STATUS[$value];
+    }
 }
--- app/Services/WorkService.php
+++ app/Services/WorkService.php
@@ -2,21 +2,31 @@
 
 namespace App\Services;
 
+use App\Models\Tag;
 use App\Models\User;
 use App\Models\Work;
 use App\Models\WorkStatement;
+use Illuminate\Support\Facades\DB;
 
 class WorkService
 {
     public function saveWork(array $data, int $user)
     {
         try {
+            DB::beginTransaction();
             $data['employer_id'] = $user;
+            $tags = isset($data['tags']) ? $this->tagsHandle($data['tags']) : [];
+            unset($data['tags']);
             $work = Work::create($data);
+            if (!empty($tags)) {
+                $work->tags()->attach($tags);
+            }
+            DB::commit();
             return ['message' => 'success', 'work' => $work, 'code' => 200];
         }
         catch (\Exception $exception)
         {
+            DB::rollBack();
             return ['message' => $exception->getMessage(), 'code' => $exception->getCode()];
         }
     }
@@ -44,5 +54,26 @@ class WorkService
             return ['data' => $exception->getMessage(), 'code' => $exception->getCode()];
         }
 
+    }
+
+    public function isNumericString($str) {
+        return ctype_digit($str);
+    }
+
+    public function tagsHandle($tags)
+    {
+        if(is_string($tags))
+        {
+            $tags = explode(',', $tags);
+        }
+        for($i = 0; $i < count($tags); $i++)
+        {
+            if(!$this->isNumericString($tags[$i]))
+            {
+                $new_tag = Tag::firstOrCreate(['name' => $tags[$i]]);
+                $tags[$i] = $new_tag->id;
+            }
+        }
+        return $tags;
     }
 }
